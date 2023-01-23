55f9840ae62213939fe6fc3e4ee6361a6f0041ce
Labhrás Mac Gabhann <coding@larrymg.me>
2023-01-23 15:21:50 -0500

    Added astro-sanity integration.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import astro from "astro-sanity";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.compatibleconnections.biz',
+  site: 'https://www.compatibleconnections.biz',
+  integrations: [astro()]
 });
