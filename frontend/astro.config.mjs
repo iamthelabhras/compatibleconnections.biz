87a0a149ba0121400ac0f81457a0357689228475
Labhrás Mac Gabhann <coding@larrymg.me>
2023-01-26 11:01:51 -0500

    Website Sanity.io project configuration.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,5 +5,11 @@ import astro from "astro-sanity";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.compatibleconnections.biz',
-  integrations: [astro()]
+  integrations: [astro(
+    {
+      projectId: "0nvd9l6m",
+      dataset: "production",
+      apiVersion: 'v2021-03-25',
+      useCdn: true,
+    })]
 });
